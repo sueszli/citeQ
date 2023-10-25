a4d744c18165f4adebb711d29f79b349d0d2ff8b
Yahya Jabary <jabaryyahya@gmail.com>
2023-10-24 23:40:59 -0400

    update

--- citeq/__main__.py
+++ citeq/__main__.py
@@ -1,13 +1,32 @@
-# 1. get researcher's papers (that have open access)
-# 2. get paper's citations
-# 3. classify type of citations: criticism, comparison, use, substantiation, basis, neutral (other)
+import asyncio
+import aiohttp
+from aiohttp import ClientSession, ClientConnectorError
 
-# apis to read from:
-# - openalex: https://docs.openalex.org/how-to-use-the-api/api-overview -> https://github.com/J535D165/pyalex
-# - semantic scholar: https://api.semanticscholar.org/api-docs/graph -> https://github.com/allenai/s2-folks/blob/main/examples/python/self_citations_on_a_paper/main.py
+import pathlib
+import sys
+
 
 import argparse
 from logger import LOG_SINGLETON as LOG, trace
+
+
+# async def fetch_html(url: str, session: ClientSession) -> tuple:
+#     try:
+#         resp = await session.request(method="GET", url=url)
+#     except ClientConnectorError:
+#         return (url, 404)
+#     return (url, resp.status)
+
+
+# async def make_requests(urls: set) -> None:
+#     async with ClientSession() as session:
+#         tasks = []
+#         for url in urls:
+#             tasks.append(fetch_html(url=url, session=session))
+#         results = await asyncio.gather(*tasks)
+
+#     for result in results:
+#         print(f"{result[1]} - {str(result[0])}")
 
 
 if __name__ == "__main__":
@@ -17,3 +36,6 @@ if __name__ == "__main__":
     parser.add_argument("field", nargs="?", help="the researchers field of study", type=str)
 
     args = parser.parse_args()
+    name_str = " ".join(args.name).strip().lower()
+
+    # asyncio.run(make_requests(urls=urls))
---
+++ diary.md
@@ -0,0 +1,14 @@
+_user flow_:
+
+0. enter researcher's name (and some hints)
+1. get researcher's papers (that have open access)
+2. get paper's citations
+3. classify type of citations: criticism, comparison, use, substantiation, basis, neutral (other)
+
+_apis_:
+
+-   semantic scholar: https://www.semanticscholar.org/product/api
+    -   https://github.com/danielnsilva/semanticscholar (⭐️ 185)
+    -   https://github.com/allenai/s2-folks/blob/main/examples/python/self_citations_on_a_paper/main.py (⭐️ 70)
+-   openalex: https://docs.openalex.org/
+    -   https://github.com/J535D165/pyalex (⭐️ 58)
