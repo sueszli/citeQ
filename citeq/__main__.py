5c6706933b8cef9229b454f10da4944dd43f041c
Yahya Jabary <jabaryyahya@gmail.com>
2023-10-24 23:17:40 -0400

    update

--- citeq/__main__.py
+++ citeq/__main__.py
@@ -1,11 +1,10 @@
-# 0. user enters researcher's name
 # 1. get researcher's papers (that have open access)
 # 2. get paper's citations
 # 3. classify type of citations: criticism, comparison, use, substantiation, basis, neutral (other)
 
 # apis to read from:
-# - openalex: https://docs.openalex.org/
-# - semantic scholar: https://www.semanticscholar.org/
+# - openalex: https://docs.openalex.org/how-to-use-the-api/api-overview -> https://github.com/J535D165/pyalex
+# - semantic scholar: https://api.semanticscholar.org/api-docs/graph -> https://github.com/allenai/s2-folks/blob/main/examples/python/self_citations_on_a_paper/main.py
 
 import argparse
 from logger import LOG_SINGLETON as LOG, trace
@@ -14,7 +13,7 @@ from logger import LOG_SINGLETON as LOG, trace
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="CiteQ: a citation analysis tool")
     parser.add_argument("name", nargs="+", help="the researchers name", type=str)
-    name_arr: list = parser.parse_args().name
-    assert name_arr is not None and len(name_arr) > 0
+    parser.add_argument("university", nargs="?", help="the researchers university", type=str)
+    parser.add_argument("field", nargs="?", help="the researchers field of study", type=str)
 
-    LOG.info(f"user requested search for: {name_arr}")
+    args = parser.parse_args()
