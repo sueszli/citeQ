377c5264ae79c036325c80b580dc2f567c61374b
Yahya Jabary <jabaryyahya@gmail.com>
2023-10-25 12:09:38 -0400

    update

--- citeq/__main__.py
+++ citeq/__main__.py
@@ -1,32 +1,7 @@
-import asyncio
-import aiohttp
-from aiohttp import ClientSession, ClientConnectorError
-
-import pathlib
-import sys
-
-
 import argparse
+import json
 from logger import LOG_SINGLETON as LOG, trace
-
-
-# async def fetch_html(url: str, session: ClientSession) -> tuple:
-#     try:
-#         resp = await session.request(method="GET", url=url)
-#     except ClientConnectorError:
-#         return (url, 404)
-#     return (url, resp.status)
-
-
-# async def make_requests(urls: set) -> None:
-#     async with ClientSession() as session:
-#         tasks = []
-#         for url in urls:
-#             tasks.append(fetch_html(url=url, session=session))
-#         results = await asyncio.gather(*tasks)
-
-#     for result in results:
-#         print(f"{result[1]} - {str(result[0])}")
+import requests
 
 
 if __name__ == "__main__":
@@ -36,6 +11,39 @@ if __name__ == "__main__":
     parser.add_argument("field", nargs="?", help="the researchers field of study", type=str)
 
     args = parser.parse_args()
-    name_str = " ".join(args.name).strip().lower()
 
-    # asyncio.run(make_requests(urls=urls))
+    # query author
+    # see: https://docs.openalex.org/api-entities/authors/author-object
+    oa_query = "https://api.openalex.org/authors?search=" + "%20".join(args.name).strip().lower()
+    oa_response = requests.get(oa_query).json()
+    oa_response_pre = json.dumps(oa_response, indent=2)
+
+    oa_meta = oa_response["meta"]
+    total = oa_meta["count"]
+    assert total > 0, f"no results found for {args.name}"
+    LOG.info(f"found {total} results in openalex")
+
+    oa_results = oa_response["results"]
+    for result in oa_results:
+        ids = result["ids"]
+        id = result["id"]
+        orcid = result["orcid"]
+        display_name = result["display_name"]
+        LOG.info(f"found {display_name}:")
+
+        # must have papers and citations
+        works_count = result["works_count"]
+        cited_by_count = result["cited_by_count"]
+        if works_count == 0 or cited_by_count == 0:
+            LOG.info(f"\tskipping due to 0 works or citations")
+            continue
+
+        # hint
+        display_name_alternatives = result["display_name_alternatives"]
+
+        # hint
+        last_known_institution = result["last_known_institution"]  # very valuable
+
+        # add to stack if matches
+        works_api_url = result["works_api_url"]  # list of all papers
+        LOG.info(f"\t see:{works_api_url}")
--- diary.md
+++ diary.md
@@ -5,10 +5,26 @@ _user flow_:
 2. get paper's citations
 3. classify type of citations: criticism, comparison, use, substantiation, basis, neutral (other)
 
-_apis_:
+<br>
 
--   semantic scholar: https://www.semanticscholar.org/product/api
+_searching the researcher's name_:
+
+1.  semantic scholar: https://www.semanticscholar.org/product/api
+
+    has more results, but also more duplicates and to little meta data about authors - so you can only guess which one is the right one
+
+    ```bash
+    curl -s "https://api.semanticscholar.org/graph/v1/author/search?query=jimmy+lin&fields=authorId,externalIds,url,name,aliases,affiliations,homepage" | jq .
+    ```
+
+    is way more popular and has a bunch of nice wrappers:
+
+    -   docs: https://api.semanticscholar.org/api-docs
     -   https://github.com/danielnsilva/semanticscholar (⭐️ 185)
     -   https://github.com/allenai/s2-folks/blob/main/examples/python/self_citations_on_a_paper/main.py (⭐️ 70)
--   openalex: https://docs.openalex.org/
+
+2.  openalex: https://docs.openalex.org/
+
+    has one great wrapper:
+
     -   https://github.com/J535D165/pyalex (⭐️ 58)
